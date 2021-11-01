f6f48df2854bc56c8c8174cc226af88d1d8db62d
Stefine <stefine.varde@gmail.com>
2021-11-01 11:10:23 +0100

    Added invoker :)

--- GameCommand/MainWindow.xaml.cs
+++ GameCommand/MainWindow.xaml.cs
@@ -17,66 +17,68 @@ namespace GameCommand
 {
     public partial class MainWindow : Window
     {
-        private ICommand _movement;
+        private ICommand _right;
+        private ICommand _left;
+        private ICommand _up;
+        private ICommand _down;
         private int _playerspeed = 50;
         private movementReceiver _receiver;
+        private movementInvoker _invoker;
+
         public MainWindow()
         {
             InitializeComponent();
             myCanvas.Focus();
             _receiver = new movementReceiver();
+            _invoker = new movementInvoker();
+            _right = new moveRight(_receiver, yellowPlayer, _playerspeed); 
+            _left = new moveLeft(_receiver, yellowPlayer, _playerspeed);
+            _up = new moveUp(_receiver, yellowPlayer, _playerspeed);
+            _down = new moveDown(_receiver, yellowPlayer, _playerspeed);
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Left)
             {
-                _movement = new moveLeft(_receiver, yellowPlayer, _playerspeed);
-                _movement.execute();
+                _invoker.setAndExecuteCommand(_left);
             }
             if (e.Key == Key.Right)
             {
-                _movement = new moveRight(_receiver, yellowPlayer, _playerspeed);
-                _movement.execute();
+                _invoker.setAndExecuteCommand(_right);
             }
             if (e.Key == Key.Up)
             {
-                _movement = new moveUp(_receiver, yellowPlayer, _playerspeed);
-                _movement.execute();
+                _invoker.setAndExecuteCommand(_up);
             }
             if (e.Key == Key.Down)
             {
-                _movement = new moveDown(_receiver, yellowPlayer, _playerspeed);
-                _movement.execute();
+                _invoker.setAndExecuteCommand(_down);
             }            
         }
 
         private void buttonA_Click(object sender, RoutedEventArgs e)
         {
-            _movement = new moveLeft(_receiver, yellowPlayer, _playerspeed);
-            _movement.execute();
+            _invoker.setAndExecuteCommand(_left);
         }
 
         private void buttonW_Click(object sender, RoutedEventArgs e)
         {
-            _movement = new moveUp(_receiver, yellowPlayer, _playerspeed);
-            _movement.execute();
+            _invoker.setAndExecuteCommand(_up);
         }
 
         private void buttonS_Click(object sender, RoutedEventArgs e)
         {
-            _movement = new moveDown(_receiver, yellowPlayer, _playerspeed);
-            _movement.execute();
+            _invoker.setAndExecuteCommand(_down);
         }
 
         private void buttonD_Click(object sender, RoutedEventArgs e)
         {
-            _movement = new moveRight(_receiver, yellowPlayer, _playerspeed);
-            _movement.execute();
+            _invoker.setAndExecuteCommand(_right);
         }
     }
 
-    public class movementReceiver
+     public class movementReceiver
     {
         public void movePlayerUp(Rectangle player, int playerSpeed)
         {
---
+++ GameCommand/MovementInvoker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCommand
+{
+    class movementInvoker
+    {
+        private ICommand _command;
+
+        public void setAndExecuteCommand(ICommand command)
+        {
+            _command = command;
+            _command.execute();
+        }
+    }
+}
